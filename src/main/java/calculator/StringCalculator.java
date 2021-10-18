d650bcb34d5d24d14616e1c8c56e20aa6b51faa1
Shivam Bhai Patel <shivam25797@gmail.com>
2021-10-18 23:03:05 +0530

    Allow the Add method to handle an unknown amount of numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,24 +1,30 @@
 package calculator;
 
+import java.util.Scanner;
+
 class StringCalculator {
 
     public int add(String input) {
+    	
         String[] str=input.split(", ");
         int[] arr=new int[str.length];
         for(int i=0; i<arr.length;i++){
             arr[i]=Integer.parseInt(str[i]);
         }
+        
+        //==============================
         int sum=0;
         if(input.length()==0){
             return 0;
         }
-        if(arr.length==1){
-            sum=  arr[0];
-        }
-        else if(arr.length==2){
-            sum=  arr[0]+arr[1];
+        else{
+            for(int i=0; i<arr.length;i++){
+                sum+=arr[i];
+            }
         }
         return sum;
     }
+
+
 
 }
