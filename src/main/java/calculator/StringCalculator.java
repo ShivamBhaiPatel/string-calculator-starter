088d73f9adf19a5acd6af9cb1a8c63d38b755d75
Shivam Bhai Patel <shivam25797@gmail.com>
2021-10-18 22:35:51 +0530

    basic addition

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,20 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        String[] str=input.split(", ");
+        int[] arr=new int[str.length];
+        for(int i=0; i<arr.length;i++){
+            arr[i]=Integer.parseInt(str[i]);
+        }
+        if(input.length()==0){
+            return 0;
+        }
+        if(arr.length==1){
+            return arr[0];
+        }
+        else{
+            return arr[0]+arr[1];
+        }
     }
 
 }
